f8230661090279c608cb46175e8a8ab853d27b66
yangDaXia <2215890610@qq.com>
2020-09-14 13:56:54 +0800

    原生Nodejs封装一个类似express框架的路由 封装post 以及通过req.body获取post的数据（3）

---
+++ express/node_dome/dome15/module/route.js
@@ -0,0 +1,60 @@
+var qureyString = require("querystring");
+var url = require("url");
+
+const servers = () => {
+
+  let G = {
+    G_GET:{},
+    G_POST:{}
+  }
+
+  const app = (req,res)=>{
+    console.log('init app');
+    const URL = url.parse(req.url,true).pathname
+    const METHOD = req.method.toLocaleUpperCase();
+
+    if (!G[`G_${METHOD}`][URL]) {
+      res.writeHead(404,{
+        "Content-Type":"text/html;charset=utf-8"
+      });
+      res.end("页面不存在");
+    }
+
+    if (METHOD === "GET" ) {
+      G_GET[URL](req,res);
+    }else{
+      let postData = "";
+      req.on('data',(data)=>{
+        postData += data
+      })
+      req.on('end',()=>{
+        req.body = qureyString.parse(postData);
+        G_POST[URL](req,res);//
+      })
+    }
+  }
+  
+  app.get = (str,cb)=>{
+    // 注册方法 
+    console.log(`${str} get __  注册成功`);
+    G.G_GET[str] = cb;
+  
+    /**
+  
+      G['/login'] = (req,res)=>{
+  
+      }
+    */
+  }
+
+  app.post = (str,cb)=>{
+    // 注册方法 
+    console.log(`${str} post __  注册成功`);
+    G.G_POST[str] = cb;
+  }
+
+  return app
+}
+
+
+module.exports = servers()
