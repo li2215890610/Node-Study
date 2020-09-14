fca681ac8433eff7b9f180fb860c3f6b0c8b2919
yangDaXia <2215890610@qq.com>
2020-09-14 11:16:23 +0800

    封装一个类似express框架的路由

---
+++ express/node_dome/dome14/03-express-router.js
@@ -0,0 +1,37 @@
+var http = require("http")
+var app = require("./module/router");
+
+// 注册web服务
+http.createServer(app).listen(10086)
+
+app.get('/',(_,res)=>{
+  
+  res.writeHead(200,{
+    "Content-Type":"text/html;charset=utf-8"
+  })
+  res.end("首页")
+})
+
+app.get('/login',(_,res)=>{
+  
+  res.writeHead(200,{
+    "Content-Type":"text/html;charset=utf-8"
+  })
+  res.end("login")
+})
+
+app.get('/news',(_,res)=>{
+  
+  res.writeHead(200,{
+    "Content-Type":"text/html;charset=utf-8"
+  })
+  res.end("news")
+})
+
+app.get('/login',(_,res)=>{
+  
+  res.writeHead(200,{
+    "Content-Type":"text/html;charset=utf-8"
+  })
+  res.end("login")
+})
---
+++ express/node_dome/dome14/README.md
@@ -0,0 +1,4 @@
+#### 封装一个类似express框架的路由
+
+[Express官网](https://www.expressjs.com.cn)
+
