de6887256b2dd39266e1c0657f62dd749d92042d
yangDaXia <2215890610@qq.com>
2020-10-13 21:28:00 +0800

    12、【Express+Mongoose】Mongoose 预定义模式修饰符 Getters与 Setters修饰符（三）

--- daDiLaoShi/Express/mongoose02/model/user.js
+++ daDiLaoShi/Express/mongoose02/model/user.js
@@ -4,7 +4,7 @@ const UserSchema = mongoose.Schema({
     //定义数据库表中的类型
     name: {
       type: String,
-      get(params){
+      get(params){ //不建议使用 暂时没有发现有啥用处
         return `a001${params}`
       }
     },
